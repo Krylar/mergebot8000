ea29695b165f0388fe155af027b1d00917343bc9
Andrew <krylar808@gmail.com>
2018-10-07 22:40:32 -0500

    0.9.4 clearflag.js: remove server roles from #merge_chat

--- commands/clearflag.js
+++ commands/clearflag.js
@@ -2,7 +2,8 @@ exports.run = async (client, message, args, level) => {
   const friendly = client.config.permLevels.find(l => l.level === level).name;
 //  message.reply(`Your permission level is: ${level} - ${friendly}`);
 
-  let tgtRole = message.guild.roles.find("name", "merging");
+  let tgtRole = message.guild.roles.find(r => r.name == "merging");
+  let mergeChat = message.guild.channels.find(c => c.name == "merge_chat");
 
   // for each role mention in message
 //  message.mentions.roles.forEach(function(arg) {
@@ -15,11 +16,20 @@ exports.run = async (client, message, args, level) => {
     membersWithRole.forEach(function(mem) {
       mem.removeRole(mRole);
     });
-    message.channel.send(`ALL users have been unflagged as @merging`);
-//  });
+
+    // for each permission overwrite in #merge_chat, do:
+    mergeChat.permissionOverwrites.forEach(p => {
+      // find all server roles with overrides for #merge_chat
+      role = message.guild.roles.get(p.id);
+      if(role.name.match(/^[0-9]/)) {
+        message.reply(`${role.name} removed from #merge_chat`);
+        p.delete();
+      }
+    });
 
 
-
+//    message.channel.send(`ALL users have been unflagged as @merging`);
+    message.channel.send(`ALL server roles have been removed from #merge_chat`);
 };
 
 exports.conf = {
