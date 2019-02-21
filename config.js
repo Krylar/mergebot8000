ab5b32f208e74f922aa9fa7b3938937791d07f9d
Glitch (mergebot8000) <none>
2019-02-21 19:20:08 +0000

    pre-0.9.5 - discrepancy in merge.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ const config = {
   "defaultSettings" : {
     "prefix": "!mg",
     "modLogChannel": "mod-log",
-    "modRole": "Moderator",
+    "modRole": "Activist",
     "adminRole": "Admin",
     "systemNotice": "true", // This gives a notice when a user tries to run a command that they do not have permission to use.
     "welcomeChannel": "welcome",
